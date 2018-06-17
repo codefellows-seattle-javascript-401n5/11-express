c75f9b040dfea599117a670ed1881a293f3f9f18
Aaron <aaronmeade9292@gmail.com>
2018-06-16 21:13:00 -0700

    added part of the readme

--- src/lib/api.js
+++ src/lib/api.js
@@ -8,68 +8,70 @@ import Foods from '../../models/foods.js';
 import fs from 'fs';
 
 let sendJSON = (res, data) => {
-    res.statusCode = 200;
-    res.statusMessage = 'good';
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify(data));
-    res.end();
+  res.statusCode = 200;
+  res.statusMessage = 'good';
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(data));
+  res.end();
 };
 
 let serverError = (res, err) => {
-    let error = {error:err};
-    res.statusCode = 500;
-    res.statusMessage = 'Server Error';
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify(error));
-    res.end();
+  let error = {
+    error: err
+  };
+  res.statusCode = 500;
+  res.statusMessage = 'Server Error';
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(error));
+  res.end();
 };
 
 router.get('/', (req, res) => {
 
-    fs.readFile('index.html', (err, data) => {
+  fs.readFile('index.html', (err, data) => {
 
-        let dataString = data.toString();
-        let filler = 'This is the homepage'
-        res.write(dataString.replace('{{filler}}', filler));
-        res.end();
+    let dataString = data.toString();
+    let filler = 'This is the homepage'
+    res.write(dataString.replace('{{filler}}', filler));
+    res.end();
 
-    });
+  });
 });
 
 router.get('/api/v1/food', (req, res) => {
-        Foods.fetchAll()
-            .then(data => sendJSON(res, data))
-            .catch(err => serverError(res, err));
+  Foods.fetchAll()
+    .then(data => sendJSON(res, data))
+    .catch(err => serverError(res, err));
 });
 
 router.get('/api/v1/food/:id', (req, res) => {
-    
-        Foods.findOne(req.params.id)
-            .then(data => sendJSON(res, data))
-            .catch(err => serverError(res, err));
-            console.log('!!!!!!!!!!!!', req.params.id);
-    
+
+  Foods.findOne(req.params.id)
+    .then(data => sendJSON(res, data))
+    .catch(err => serverError(res, err));
+  console.log('!!!!!!!!!!!!', req.params.id);
+
 });
 
 router.delete('api/v1/food/:id', (req, res) => {
-    if(req.params.id){
-        Foods.deleteOne(req.params.id)
-            .then(() => {
-                res.statusCode = 204;
-                res.statusMessage = 'success';
-                res.write(req.params.id, ': was deleted');
-                res.end();
-            })
-            .catch(console.error);
-    }
+  if (req.params.id) {
+    Foods.deleteOne(req.params.id)
+      .then(() => {
+        res.statusCode = 204;
+        res.statusMessage = 'success';
+        res.write(req.params.id, ': was deleted');
+        res.end();
+      })
+      .catch(console.error);
+  }
 });
 
-router.post('/api/v1/food', (req, res) => {  
-      let newFoods = new Foods(req.body);
-  
-      newFoods.save()
-        .then(data => sendJSON(res, data))
-        .catch(console.error);
-  });
+router.post('/api/v1/food', (req, res) => {
+  let newFoods = new Foods(req.body);
+
+  newFoods.save()
+    .then(data => sendJSON(res, data))
+    .catch(console.error);
+});
 
 export default router;
